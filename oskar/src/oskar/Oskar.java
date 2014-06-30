43808abc33e96a4675bf92561a1bf3123e3c7f02
Marcus Warm <Marcus.Warm@geneva-id.com>
2014-06-30 10:57:45 +0200

    Git

---
+++ oskar/src/oskar/Oskar.java
@@ -0,0 +1,15 @@
+package oskar;
+
+/**
+ * Oskar aus der Mülltonne
+ * <p>Dieses Tool erinnert per Email an das Heraus- und Hereinstellen der Mülltonnen.
+ * 
+ * @author Marcus Warm
+ * @since  30.06.2014
+ */
+public class Oskar {
+
+	public static void main(String[] args) {
+		System.out.println("Oskar aus der Muelltonne");
+	}
+}
